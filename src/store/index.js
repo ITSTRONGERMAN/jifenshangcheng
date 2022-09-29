87f0789c9654dad63f63b7ea1011e837f8553772
Pioneer <2929727886@qq.com>
2022-09-30 00:16:07 +0800

    表单信息验证与提示

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import ShowLoginModal from './ShowLoginModal'
-import LoginStatus from './LoginStatus'
-import ShowTips from './ShowTips'
-Vue.use(Vuex)
+import Vue from "vue";
+import Vuex from "vuex";
+import ShowLoginModal from "./ShowLoginModal";
+import LoginStatus from "./LoginStatus";
+import ShowTips from "./ShowTips";
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     ShowLoginModal,
     LoginStatus,
-    ShowTips
-  }
-})
+    ShowTips,
+  },
+});
